072b1041fd0d4ae467c919206b86ef844808d8e0
heckert-gamedev <unity.github@nostromo.in-berlin.de>
2023-08-24 00:18:11 +0200

    Implement locomotion logic to push crates and give player free movement over sand tiles

--- Paco-Cabana/Assets/Scripts/Player/PlayerLocomotion.cs
+++ Paco-Cabana/Assets/Scripts/Player/PlayerLocomotion.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using DG.Tweening;
-using System;
 using UnityEngine.Tilemaps;
-using UnityEditor;
-using UnityEditor.Compilation;
 
 namespace he
 {
@@ -16,6 +13,13 @@ namespace he
         [SerializeField] float moveSpeed = 1f;
         [SerializeField] float gridLength = 1f;
         [SerializeField] Tilemap pathWays;
+        [SerializeField] Tilemap walkabilityMap;
+
+        [SerializeField] Tile pathWalkable;
+        [SerializeField] Tile pathBlocked;
+        [SerializeField] Tile pathExtra;
+
+        [SerializeField] LayerMask layerMask;
 
 
         Rigidbody2D rb;
@@ -40,7 +44,7 @@ namespace he
                 Vector3 myMovement = inputHandler.MoveVector.normalized * gridLength;
                 Vector2 targetPosition = new(myPosition.x + myMovement.x, myPosition.y + myMovement.y);
 
-                if (CheckDestinationIsWalkable(targetPosition))
+                if (CheckDestinationIsWalkableAndPushCrate(targetPosition))
                 {
                     rb.DOMove(targetPosition, .25f, false).OnComplete(AlignPositionToGrid);
                 }
@@ -48,17 +52,60 @@ namespace he
         }
 
 
-        private bool CheckDestinationIsWalkable(Vector2 targetPosition)
+        private bool CheckDestinationIsWalkableAndPushCrate(Vector2 targetPosition)
         {
-            TileBase tile = pathWays.GetTile(pathWays.WorldToCell(targetPosition));
-            Debug.Log($"tile found at {targetPosition}: {tile?.name}");
 
-            return pathWays.HasTile(pathWays.WorldToCell(targetPosition));
+            TileBase tile = walkabilityMap.GetTile(walkabilityMap.WorldToCell(targetPosition));
+        
+            //Debug.Log($"tile found at {targetPosition}: {tile?.name}");
+            if (tile.name == pathWalkable.name)
+            {
+                return true;
+            }
+            else
+            if (tile.name == pathBlocked.name)
+            {
+                return false;
+            }
+            else
+            {
+                // case: pathExtra
+                //Debug.DrawRay(targetPosition, inputHandler.MoveVector, Color.magenta, 5f);
+                RaycastHit2D hit = Physics2D.CircleCast(targetPosition, .5f, inputHandler.MoveVector, 1f, layerMask);
+
+                //Debug.Log($"hit: {hit.transform?.name}, unhit: {unhit.transform?.name}");
+                if (hit)
+                {
+                    //Debug.Log($"Ouch! {hit.transform.name}... LM: {layerMask.value}");
+                    Transform hitObject = hit.transform.parent;
+
+                    if (hitObject.TryGetComponent<CrateMovementHandler>(out CrateMovementHandler cmhComponent))
+                    {
+                        //Debug.Log($"cmh {cmhComponent.name}");
+                        Vector3 myMovement = inputHandler.MoveVector.normalized * gridLength;
+                        Vector2 targetTargetPosition = new(targetPosition.x + myMovement.x, targetPosition.y + myMovement.y);
+                        //Debug.Log($"probing ttp {targetTargetPosition}");
+                        if (cmhComponent.CheckDestinationCellIsValid(walkabilityMap, targetTargetPosition))
+                        {
+                            return cmhComponent.PushCrate(targetTargetPosition);
+                        }
+                        else
+                        {
+                            return false;
+                        }
+
+                    }
+                }
+
+                return true;
+            }
+
+            //return pathWays.HasTile(pathWays.WorldToCell(targetPosition));
         }
 
         private void AlignPositionToGrid()
         {
-            Debug.Log($"Aligning position");
+            //Debug.Log($"Aligning position");
             rb.DOMove(rb.position, .01f, true);
         }
     }
